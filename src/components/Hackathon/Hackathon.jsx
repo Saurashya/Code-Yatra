be272beafa817cda34a840e928f8fa0995a86867
saurashya <saurashya1@gmail.com>
2025-01-14 19:30:05 +0545

    Fix deployment bugs

--- src/components/Hackathon/Hackathon.jsx
+++ src/components/Hackathon/Hackathon.jsx
@@ -4,8 +4,7 @@ import img from '../../assets/hackathon.png'
 const Hackathon = () => {
   return (
     <div className='h-[100vh] w-[100vw] flex flex-col justify-start items-center gap-4 '>
-        <img src="https://drive.google.com/uc?export=view&id=1IPkNPL85YRpCV7pCCxP8mzfw8NXX3Mgu
-" alt="" className='h-[80vh] w-[45vw] rounded-xl z-20'/>
+        <img src={img} alt="" className='h-[80vh] w-[45vw] rounded-xl z-20'/>
         <button className='bg-orange-500 p-1 rounded-lg cursor-pointer px-3 font-light tracking-wider hover:bg-gradient-to-br from-purple-500 to-indigo-400 z-20 text-white text-xl'>Click Here For Registration</button>
     </div>
   )
--- src/pages/OC.jsx
+++ src/pages/OC.jsx
@@ -9,12 +9,9 @@ const OC = () => {
         <h1 className='text-center text-4xl text-white font-semibold tracking-wider my-12'>CodeYatra Organizing Club</h1>
         <div className="club-logo grid grid-cols-3 gap-12">
             {/* <div></div> */}
-            <img src="https://drive.google.com/uc?export=view&id=13sX6NOOmhkEejxVDuoVWZTogp8sIPdBh
-" alt="It club Logo"className='h-[70%] rounded-full mx-12 z-20' />
-            <img src="https://drive.google.com/uc?export=view&id=15Eq7mjqi_1NOdj2ERaLmuB0Rwy4DR_DE
-" alt="Hecc Logo" className='h-[70%] rounded-full z-20'/>
-            <img src="https://drive.google.com/uc?export=view&id=1_I5O4UIM2LwG9ixTFZO3MGJ_ZalP0KHT
-" alt=" HRC logo" className='h-[70%] rounded-full z-20' />
+            <img src={Itclub} alt="It club Logo"className='h-[70%] rounded-full mx-12 z-20' />
+            <img src={Hecc} alt="Hecc Logo" className='h-[70%] rounded-full z-20'/>
+            <img src={Hrc} alt=" HRC logo" className='h-[70%] rounded-full z-20' />
             {/* <div></div> */}
         </div>
     </div>
